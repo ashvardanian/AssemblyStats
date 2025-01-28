3a3a4deb773a1d7c786a9f996717de14bcf435a1
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2025-01-28 10:28:25 +0000

    Docs: Deprecate downloading remote packs

--- gather_conda.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import conda
-from conda.api import Channel, Solver
-
-if __name__ == "__main__":
-    channels = Channel("conda-forge")
-    solver = Solver("zlib", channels=channels)
